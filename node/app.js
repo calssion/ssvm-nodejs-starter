d11f4f35cd4d94d0c476fb29970bd7547b2b730d
calssion <1170804131@qq.com>
2020-08-15 15:57:25 +0800

    first_try

--- node/app.js
+++ node/app.js
@@ -1,19 +1,90 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+// 监听并返回字符串
+// const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+
+let btn1 = document.getElementById('btn');
+ 
+btn1.addEventListener('click', function() {
+    ajax('POST', "http://127.0.0.1:3000/", 'name='+this.innerHTML);
+});
+ 
+// 封装的ajax方法
+function ajax(method, url, val) {  // 方法，路径，传送数据
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                // content.innerHTML = xhr.responseText;
+                var image = new Image();
+                image.src= this.responseText;
+                document.getElementById("image").innerHTML = image.outerHTML;
+            } else {
+                alert('Request was unsuccessful: ' + xhr.status);
+            }
+        }
+    };
+ 
+    xhr.open(method, url, true);   
+    if(val) {
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); //设置POST 较常见表单的提交数据方式
+    }
+    xhr.send(val);
+}
 
 const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
-const port = 3000;
+// const url = require('url');
+let qs = require('querystring');
+// const hostname = '0.0.0.0';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
-});
+// const server = http.createServer((req, res) => {
+//   const queryObject = url.parse(req.url,true).query;
+//   if (!queryObject['name']) {
+//     res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+//   } else {
+//     res.end(say(queryObject['name']) + '\n');
+//   }
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+var width_num = 2850;
+var height_num = 1900;
+//该函数用来创建一个HTTP服务器，并将 requestListener 作为 request 事件的监听函数。req请求对象，res响应对象
+http.createServer(function(req, res) {   
+    let body = '';  // 一定要初始化为"" 不然是undefined
+    req.on('data', function(data) {
+        body += data; // 所接受的Json数据
+    });
+    req.on('end', function() { 
+        res.writeHead(200, {  // 响应状态
+            "Content-Type": "text/plain",  // 响应数据类型
+            'Access-Control-Allow-Origin': '*'  // 允许任何一个域名访问
+        });
+        if(qs.parse(body).name == 'resize') {
+            if(width_num>0){ 
+                // 对图片进行裁剪
+                const { resize_file } = require('../pkg/ssvm_nodejs_starter_lib.js');
+      
+                const dim = {
+                    width: width_num,
+                    height: height_num
+                };
+
+                resize_file(JSON.stringify([dim, 'cat.png', `test.png`]));
+                width_num -= 5;
+                height_num -= 5;
+                res.write('test.png');
+            }
+        }
+        res.end();
+    });   
+}).listen(3000);
+
+// 获取 html 的内容
+// const { fetch, download } = require('../pkg/ssvm_nodejs_starter_lib.js');
+  
+// fetch("https://raw.githubusercontent.com/second-state/nodejs-helper/master/LICENSE");
+// download("https://www.secondstate.io/", "test.html");
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,88 @@
+use std::io::{BufReader, Cursor};
 use wasm_bindgen::prelude::*;
+use nodejs_helper;
+use serde::{Serialize, Deserialize};
+use image::{GenericImageView, png, ImageEncoder, ImageFormat};
+use image::imageops::FilterType;
 
 #[wasm_bindgen]
 pub fn say(s: &str) -> String {
   println!("The Rust function say() received {}", s);
   let r = String::from("hello ");
   return r + s;
+}
+
+// #[wasm_bindgen]
+// pub fn fetch(url: &str) {
+//   let content = nodejs_helper::request::fetch_as_string(url);
+//   nodejs_helper::console::log(url);
+//   nodejs_helper::console::log(&content);
+// }
+
+#[derive(Serialize, Deserialize)]
+#[derive(Copy, Clone, Debug)]
+pub struct Dimension {
+  pub width: u32,
+  pub height: u32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Picture {
+  pub dim: Dimension,
+  pub raw: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+  pub id: u32,
+  pub full_name: String,
+  pub created: String,
+}
+
+#[wasm_bindgen]
+pub fn resize_file(input: &str) {
+  let p: (Dimension, String, String) = serde_json::from_str(input).unwrap();
+
+  nodejs_helper::console::time("Resize file");
+  let raw = nodejs_helper::fs::read_file_sync(&p.1);
+  nodejs_helper::console::time_log("Resize file", "Done reading");
+  let src = Picture {
+    dim: p.0,
+    raw: raw,
+  };
+  let target = resize_impl(&src);
+  nodejs_helper::console::time_log("Resize file", "Done resizing");
+
+  nodejs_helper::fs::write_file_sync(&p.2, &target.raw);
+  nodejs_helper::console::time_log("Resize file", "Done writing");
+
+  nodejs_helper::console::time_end("Resize file");
+}
+
+pub fn resize_impl(src: &Picture) -> Picture {
+  let cur = Cursor::new(&src.raw);
+
+  let fin = BufReader::new(cur);
+
+  // load the `image::DynamicImage`
+  let mut img = image::load(fin, ImageFormat::Png).unwrap();
+
+  // Resize the img in the memory.
+  img = img.resize_to_fill(src.dim.width, src.dim.height, FilterType::Lanczos3);
+
+  let (w, h) = img.dimensions();
+
+  // Write the resized image to the vector.
+  let mut cur: Cursor<Vec<u8>>  = Cursor::new(vec![]);
+  png::PNGEncoder::new(&mut cur).write_image(&img.to_bytes(), w, h, img.color()).unwrap();
+
+  let dim = Dimension {
+    width: w,
+    height: h,
+  };
+
+  Picture {
+    dim,
+    raw: cur.into_inner(),
+  }
 }
